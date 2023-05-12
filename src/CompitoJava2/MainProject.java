d7a71ba721c197611081da48563ac7b7ed74d01c
sabrizlt <sabrizaltini6@gmail.com>
2023-05-12 17:19:24 +0200

    ok

--- src/CompitoJava2/MainProject.java
+++ src/CompitoJava2/MainProject.java
@@ -1,10 +1,13 @@
 package CompitoJava2;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,36 +19,36 @@ public class MainProject {
 
 	public static void main(String[] args) {
 
-		catalogo.add(new Libro("1L1L", "Lo straniero", 1942, 400, "Albert Camus", "Romanzo"));
-		catalogo.add(new Libro("2L", "Alla ricerca del tempo perduto", 1927, 600, "Marcel Proust", "Romanzo"));
-		catalogo.add(new Libro("3L", "Il processo", 1925, 400, "Franz Kafka", "Romanzo"));
-		catalogo.add(new Libro("4L", "Il piccolo principe", 1943, 500, "Antoine Saint-Exupery", "Racconto"));
-		catalogo.add(new Libro("5L", "Furore", 1939, 200, "John Steinbeck", "Romanzo"));
+		catalogo.add(new Libro("L1234O", "Lo straniero", 1942, 150, "Albert Camus", "Romanzo"));
+		catalogo.add(new Libro("L5678O", "Alla ricerca del tempo perduto", 1927, 250, "Marcel Proust", "Romanzo"));
+		catalogo.add(new Libro("L9012O", "Il processo", 1925, 350, "Franz Kafka", "Romanzo"));
+		catalogo.add(new Libro("L3456O", "Il piccolo principe", 1943, 450, "Antoine Saint-Exupery", "Racconto"));
+		catalogo.add(new Libro("L7890O", "Furore", 1939, 550, "John Steinbeck", "Romanzo"));
 
-		catalogo.add(new Rivista("1R", "Focus", 2010, 120, Periodo.SETTIMANALE));
-		catalogo.add(new Rivista("2R", "NYMagazine", 2006, 150, Periodo.MENSILE));
-		catalogo.add(new Rivista("3R", "Vanity Fair", 2005, 200, Periodo.SEMESTRALE));
-		catalogo.add(new Rivista("4R", "Vogue", 2010, 100, Periodo.MENSILE));
-		catalogo.add(new Rivista("5R", "Giallo Zafferano", 2012, 50, Periodo.SETTIMANALE));
+		catalogo.add(new Rivista("R1234A", "Focus", 2010, 120, Periodo.SETTIMANALE));
+		catalogo.add(new Rivista("R5678A", "NYMagazine", 2006, 150, Periodo.MENSILE));
+		catalogo.add(new Rivista("R9012A", "Vanity Fair", 2005, 200, Periodo.SEMESTRALE));
+		catalogo.add(new Rivista("R3456A", "Vogue", 2010, 100, Periodo.MENSILE));
+		catalogo.add(new Rivista("R7890A", "Giallo Zafferano", 2012, 50, Periodo.SETTIMANALE));
 
 		log.info("ARCHIVIO");
 		catalogo.forEach(e -> System.out.println(e));
 
 		log.info("RIMOZIONE ELEMENTI CON ISBN");
-		String elementoRimosso = removeElement("3L");
+		String elementoRimosso = removeElement("L9012O");
 		log.info("Elemento rimosso" + elementoRimosso);
 
 		log.info("RICERCA ELEMENTI CON ISBN");
-		String ricercaPerIsbn = cercaISBN("3R").toString();
-		log.info("Elemento ricercato per codice ISBN" + ricercaPerIsbn);
+		String ricercaIsbn = cercaISBN("R7890A").toString();
+		log.info("Elemento ricercato per codice ISBN" + ricercaIsbn);
 
 		log.info("RICERCA ELEMENTI PER ANNO");
-		String ricercaPerAnnoPub = cercaAnno(2005).toString();
-		log.info("Elemento ricercato per anno pubblicazione" + ricercaPerAnnoPub);
+		String ricercaAnno = cercaAnno(2005).toString();
+		log.info("Elemento ricercato per anno pubblicazione" + ricercaAnno);
 
 		log.info("RICERCA ELEMENTI PER AUTORE");
-		String ricercaPerAutore = cercaAutore("John Steinbeck").toString();
-		log.info("Elemento ricercato per Autore" + ricercaPerAutore);
+		String ricercaAutore = cercaAutore("John Steinbeck").toString();
+		log.info("Elemento ricercato per Autore" + ricercaAutore);
 
 	}
 
@@ -76,8 +79,11 @@ public class MainProject {
 				.collect(Collectors.toList());
 	}
 
-	
-	
+	public static void scriviSuFile() throws IOException {
+		FileUtils.writeStringToFile(file, catalogo.toString(), "UTF-8", true);
+		System.out.println("Testo scritto su file");
+	}
+
+
 
 }
-
